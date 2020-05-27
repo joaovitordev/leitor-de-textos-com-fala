0fc39f8a9649c6bec3410a2bb78472540f1caa4b
João Vitor <jv17fc@gmail.com>
2020-05-27 18:39:25 -0300

    Utilizando metodo speak para ouvir textos clicados

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ const humanExpressions = [
     { img: './img/grandma.jpg', text: 'Quero ver a vovó' }
 ]
 
+const utterance = new SpeechSynthesisUtterance()
+
+const setTextMessage = text => {
+    utterance.text = text
+}
+
+const speakText = () => {
+    speechSynthesis.speak(utterance)
+}
+
 const createExpressionBox = ({ img, text }) => {
     const div = document.createElement('div')
 
@@ -27,6 +37,16 @@ const createExpressionBox = ({ img, text }) => {
         <img src="${img}" alt="${text}">
         <p class="info">${text}</p>
     `
+
+    div.addEventListener('click', () => {
+        setTextMessage(text)
+        speakText()
+
+        div.classList.add('active')
+        setTimeout(() => {
+            div.classList.remove('active')
+        }, 1000)
+    })
 
     main.appendChild(div)
 }
