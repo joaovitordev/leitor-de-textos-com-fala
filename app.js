6c51d51e70fbfa5b2f39b55b8c8d876f70c06d55
João Vitor <jv17fc@gmail.com>
2020-05-27 17:55:24 -0300

    Inserindo dados do array no HTML com o DOM

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const main = document.querySelector('main')
+
 const humanExpressions = [
     { img: './img/drink.jpg', text: 'Estou com sede' },
     { img: './img/food.jpg', text: 'Estou com fome' },
@@ -10,5 +12,19 @@ const humanExpressions = [
     { img: './img/outside.jpg', text: 'Quero ir lá fora' },
     { img: './img/home.jpg', text: 'Quero ir para casa' },
     { img: './img/school.jpg', text: 'Quero ir para a escola' },
-    { img: './img/grandma.jpg', text: 'Quero ver a vovó' },
+    { img: './img/grandma.jpg', text: 'Quero ver a vovó' }
 ]
+
+const createExpressionBox = ({ img, text }) => {
+    const div = document.createElement('div')
+
+    div.classList.add('expression-box')
+    div.innerHTML = `
+        <img src="${img}" alt="${text}">
+        <p class="info">${text}</p>
+    `
+
+    main.appendChild(div)
+}
+
+humanExpressions.forEach(createExpressionBox)
